ef9867320666644af0adbbaea22adf1e55733b49
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2025-12-04 23:09:02 +0500

    add controller and service file

--- life-cycle-events/src/app.module.ts
+++ life-cycle-events/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseController } from './database/database.controller';
+import { DatabaseService } from './database/database.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, DatabaseController],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
